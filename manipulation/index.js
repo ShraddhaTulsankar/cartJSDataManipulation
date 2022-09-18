6772131816287f6b02ca1093b6d7427590992f21
karankhot <karan.khot@optum.com>
2022-09-18 01:10:31 -0500

    printAllCats HOF

--- manipulation/index.js
+++ manipulation/index.js
@@ -1,58 +1,45 @@
-const BuyAgainArr = require('../dataset/BuyAgain.json');
-const PantryArr = require('../dataset/Pantry.json');
-const ProduceArr = require('../dataset/Produce.json');
-const SnacksArr = require('../dataset/Snacks.json');
+let BuyAgainArr = require('../dataset/BuyAgain.json');
+let PantryArr = require('../dataset/Pantry.json');
+let ProduceArr = require('../dataset/Produce.json');
+let SnacksArr = require('../dataset/Snacks.json');
+const { updateObjectProperties } = require('../utils/objectManipulations');
 
-const updateObjectProperties = (itemInitialPrice) => {
-  let random = Math.floor(Math.random() * 10);
-  itemInitialPrice = parseFloat(itemInitialPrice.replace(/\$/g, ''));
-  let itemLowerLimitPrice = itemInitialPrice * 0.8;
-  let itemHigherLimitPrice = itemInitialPrice * 1.5;
-  let itemCategories =
-    random >= 0 && random < 3
-      ? 'snacks'
-      : random >= 3 && random < 6
-      ? 'pantry'
-      : random >= 6 && random < 9
-      ? 'produce'
-      : 'buyAgain';
-  let isAvailable = random % 2 == 0 ? true : false;
-  let deliveryTime = random == 0 ? 'Same Day Delivery' : random;
-  return [
-    itemInitialPrice,
-    itemLowerLimitPrice,
-    itemHigherLimitPrice,
-    itemCategories,
-    isAvailable,
-    deliveryTime,
-  ];
+const refactorObject = (arr) => {
+  const newObject = arr.map((item, index) => {
+    let { itemPrice, itemCategory, ...rest } = item;
+    const [
+      itemInitialPrice,
+      itemLowerLimitPrice,
+      itemHigherLimitPrice,
+      category,
+      isAvailable,
+      deliveryTime,
+    ] = updateObjectProperties(itemPrice);
+    let itemCosts = {
+      itemInitialPrice: itemInitialPrice,
+      itemLowerLimitPrice: itemLowerLimitPrice,
+      itemHigherLimitPrice: itemHigherLimitPrice,
+    };
+    let itemCategories = [category, itemCategory];
+    let itemMetadata = { isAvailable: isAvailable, deliveryTime: deliveryTime };
+    return {
+      id: index,
+      ...rest,
+      itemCosts,
+      itemCategory,
+      itemCategories,
+      itemMetadata,
+    };
+  });
+  return newObject;
 };
 
-const newObject = SnacksArr.map((item, index) => {
-  let { itemPrice, itemCategory, ...rest } = item;
-  const [
-    itemInitialPrice,
-    itemLowerLimitPrice,
-    itemHigherLimitPrice,
-    category,
-    isAvailable,
-    deliveryTime,
-  ] = updateObjectProperties(itemPrice);
-  let itemCosts = {
-    itemInitialPrice: itemInitialPrice,
-    itemLowerLimitPrice: itemLowerLimitPrice,
-    itemHigherLimitPrice: itemHigherLimitPrice,
-  };
-  let itemCategories = [category, itemCategory];
-  let itemMetadata = { isAvailable: isAvailable, deliveryTime: deliveryTime };
-  return {
-    id: index,
-    ...rest,
-    itemCosts,
-    itemCategory,
-    itemCategories,
-    itemMetadata,
-  };
-});
+const printAllCategories = (callbackfunction) => {
+  BuyAgainArr = callbackfunction(BuyAgainArr);
+  PantryArr = callbackfunction(PantryArr);
+  ProduceArr = callbackfunction(ProduceArr);
+  SnacksArr = callbackfunction(SnacksArr);
+  console.log(BuyAgainArr, PantryArr, ProduceArr, SnacksArr);
+};
 
-console.log(newObject);
+printAllCategories(refactorObject);
