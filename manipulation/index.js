c1d92006e1d575dc808be4f3c17b2a2891de4f1d
Shraddha Tulsankar <st54723@gmail.com>
2022-09-18 01:31:59 -0400

    Object Refine

--- manipulation/index.js
+++ manipulation/index.js
@@ -3,6 +3,31 @@ const PantryArr = require('../dataset/Pantry.json');
 const ProduceArr = require('../dataset/Produce.json');
 const SnacksArr = require('../dataset/Snacks.json');
 
+const updateObjectProperties = (itemInitialPrice) => {
+  let random = Math.floor(Math.random() * 10);
+  itemInitialPrice = parseFloat(itemInitialPrice.replace(/\$/g, ''));
+  let itemLowerLimitPrice = itemInitialPrice * 0.8;
+  let itemHigherLimitPrice = itemInitialPrice * 1.5;
+  let itemCategories =
+    random >= 0 && random < 3
+      ? 'snacks'
+      : random >= 3 && random < 6
+      ? 'pantry'
+      : random >= 6 && random < 9
+      ? 'produce'
+      : 'buyAgain';
+  let isAvailable = random % 2 == 0 ? true : false;
+  let deliveryTime = random == 0 ? 'Same Day Delivery' : random;
+  return [
+    itemInitialPrice,
+    itemLowerLimitPrice,
+    itemHigherLimitPrice,
+    itemCategories,
+    isAvailable,
+    deliveryTime,
+  ];
+};
+
 const newObject = SnacksArr.map((item, index) => {
   let { itemPrice, itemCategory, ...rest } = item;
   const [
@@ -29,31 +54,5 @@ const newObject = SnacksArr.map((item, index) => {
     itemMetadata,
   };
 });
-
-function updateObjectProperties(itemInitialPrice) {
-  let random = Math.floor(Math.random() * 10);
-  itemInitialPrice = itemInitialPrice.replace(/\$/g, '');
-  itemInitialPrice = parseFloat(itemInitialPrice);
-  let itemLowerLimitPrice = itemInitialPrice * 0.8;
-  let itemHigherLimitPrice = itemInitialPrice * 1.5;
-  let itemCategories =
-    random >= 0 && random < 3
-      ? 'snacks'
-      : random >= 3 && random < 6
-      ? 'pantry'
-      : random >= 6 && random < 9
-      ? 'produce'
-      : 'buyAgain';
-  let isAvailable = random % 2 == 0 ? true : false;
-  let deliveryTime = random == 0 ? 'Same Day Delivery' : random;
-  return [
-    itemInitialPrice,
-    itemLowerLimitPrice,
-    itemHigherLimitPrice,
-    itemCategories,
-    isAvailable,
-    deliveryTime,
-  ];
-}
 
 console.log(newObject);
